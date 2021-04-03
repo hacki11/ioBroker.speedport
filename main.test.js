f6047a0ad428da8af22d4a0c50d7b4db3f1769bd
Juergen Schmid <jur.schmid@gmail.com>
2021-04-03 18:51:44 +0200

    merged unit tests because issue with axios mock during npm run

--- main.test.js
+++ main.test.js
@@ -4,7 +4,7 @@
  * This is a dummy TypeScript test file using chai and mocha
  *
  * It's automatically excluded from npm and its build output is excluded from both git and npm.
- * It is advised to test all your modules with accompanying *.test.js-files
+ * It is advised to test all your modules with accompanying *.unit.js-files
  */
 
 // tslint:disable:no-unused-expression
